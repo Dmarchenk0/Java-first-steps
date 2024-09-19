8b6b73f669c4a9b51782de3f0dc409689880c659
Dmitrii <glele@LAPTOP-GJATDHDQ.(none)>
2024-09-19 20:55:23 +0300

    Homework 1, task 3("Point")

---
+++ src/main/java/org/example/HW1/Point.java
@@ -0,0 +1,42 @@
+package org.example.HW1;
+
+public class Point implements Movable{
+    private int X;
+    private int Y;
+
+    public int getX() {
+        return this.X;
+    }
+
+    public int getY() {
+        return this.Y;
+    }
+
+    public void setX(int x) {
+        this.X = x;
+    }
+
+    public void setY(int y) {
+        this.Y = y;
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
